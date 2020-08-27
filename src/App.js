21b5fc9bd6bc51879c8e6a0f80d4677b33bf42a7
samueljlopes <samueljlopes@outlook.com>
2020-08-27 16:07:54 +0100

    Adding react.js, paper.js integration and mouse events
    Readme.md still needs to be updated

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
 import React, { Component } from 'react';
 import paper from 'paper'
 import './App.css';
+import InkCanvas from './core/ink'
 
 class App extends Component {
   componentDidMount() 
   {
-    paper.setup('canvas') //This sets up the canvas which will then be drawn on
-    require("./core/ink.js")
+    paper.setup('canvas')
+    //This sets up the canvas which will then be drawn on
   } 
 
   render() {
     return (
       <div>
-        <canvas id="canvas"></canvas>
+        <canvas id="canvas" resize="true"></canvas>
+        <InkCanvas/>
       </div>
     ); //The canvas is returned as a DOM element
   }
--- src/core/ink.js
+++ src/core/ink.js
@@ -1,6 +1,51 @@
-import { paper } from 'paper';
-let circle = new paper.Path.Circle({
-    center: paper.view.center,
-    radius: 50,
-    fillColor: 'orange'
-  });
+import paper, {Point, Path, Circle} from 'paper';
+import React from 'react';
+
+class InkCanvas extends React.Component
+{
+  state = {isMouseDown : false}
+  //State is effectively a property outside the data flow, not an argument passed in
+  constructor (props)
+  {
+    super(props);
+    this.expansionRate = props.expansionRate;
+  }
+
+  componentDidMount() 
+  {
+    var canvas = document.getElementById('canvas'); //canvas is found in document
+		paper.setup(canvas);
+    let path = new Path();
+    path.strokeColor = 'black';
+    path.add(new Point(0,0), 
+            new Point(2 * paper.view.center.x, 0), 
+            new Point(2 * paper.view.center.x, 2 * paper.view.center.y), 
+            new Point(0, 2 * paper.view.center.y), 
+            new Point(0,0));
+    
+    setInterval(() => this.frameUpdate(), 1);
+  }
+
+  frameUpdate() 
+  {
+    paper.view.onMouseDown = (event) => {
+      this.state.isMouseDown = true;
+    }
+
+    paper.view.onMouseUp = (event) => {
+      this.state.isMouseDown = false;
+    }
+    console.log(this.state.isMouseDown);
+  }
+
+  render() 
+  {
+    return(
+      <div>
+        <canvas id = "canvas"></canvas> 
+      </div>
+    ); //canvas is returned to the main program
+  }
+}
+
+export default InkCanvas
