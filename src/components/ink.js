737bda610568f5be861594b1bc0b6e242b3e94f4
samueljlopes <samueljlopes@outlook.com>
2020-08-29 00:52:27 +0100

    Revert "Revert "First test for radial displacement""
    This reverts commit bb8dd6472d02cc5e22d7d5bef00b9b262f27fc02.

--- src/components/ink.js
+++ src/components/ink.js
@@ -1,75 +1,78 @@
-import paper, {Point, Path, Circle, Group} from 'paper';
+import paper, { Point, Path, Circle, Group } from 'paper';
 import React from 'react';
-import math from 'mathjs';
+import { create, all } from 'mathjs'
 
-class InkCanvas extends React.Component
-{
-  state = {isMouseDown : false,
-          allItems: paper.Group}
+const config = {}
+const math = create(all, config)
+
+class InkCanvas extends React.Component {
+  state = {
+    isMouseDown: false,
+    allItems: paper.Group
+  }
   //State is effectively a property outside the data flow, not an argument passed in
-  constructor (props)
-  {
+  constructor(props) {
     super(props);
-    this.expansionRate = parseInt(props.expansionRate,10);
+    this.expansionRate = parseInt(props.expansionRate, 10);
   }
 
-  componentDidMount() 
-  {
+  componentDidMount() {
     this.setupCanvas();
     let path = new Path(); //This is to test the size of the canvas 
     path.strokeColor = 'black';
-    path.add(new Point(0,0), 
-            new Point(2 * paper.view.center.x, 0), 
-            new Point(2 * paper.view.center.x, 2 * paper.view.center.y), 
-            new Point(0, 2 * paper.view.center.y), 
-            new Point(0,0));
+    path.add(new Point(0, 0),
+      new Point(2 * paper.view.center.x, 0),
+      new Point(2 * paper.view.center.x, 2 * paper.view.center.y),
+      new Point(0, 2 * paper.view.center.y),
+      new Point(0, 0));
     setInterval(() => this.frameUpdate(), 10);
   }
 
-  setupCanvas() 
-  {
+  setupCanvas() {
     paper.setup('canvas');
-    this.setState({allItems : new Group()})
+    this.setState({ allItems: new Group() })
   }
 
-  addItemToAllItems(paperItem) 
-  {
+  addItemToAllItems(paperItem) {
     let currentAllItems = this.state.allItems;
     currentAllItems.addChild(paperItem);
-    this.setState({allItems : currentAllItems});
+    this.setState({ allItems: currentAllItems });
   }
 
-  frameUpdate() 
-  {
+  frameUpdate() {
     paper.view.onMouseDown = (event) => {
-      if (this.state.allItems.children.length > 0) 
-      {
-        let length = this.state.allItems.children.length;
-      }
-      let circle = new Path.Circle({center : event.point, radius : 50, fillColor : 'black'})
+      let circle = new Path.Circle({ center: event.point, radius: 10, fillColor: 'black' })
       circle.flatten(0.1); //This is a bit of a cheat. The number of anchor points on the circle primitive is 4, so the flatten command establishes more anchor points.
-      //circle.selected = true;
+      circle.selected = true;
 
-      this.setState({ isMouseDown : true });
+      this.setState({ isMouseDown: true });
       this.addItemToAllItems(circle);
     }
 
     paper.view.onMouseUp = (event) => {
-      this.setState({ isMouseDown : false });
+      this.setState({ isMouseDown: false });
     }
-    
-    if (this.state.isMouseDown && this.state.allItems.children.length > 0)
-    {
+
+    if (this.state.isMouseDown && this.state.allItems.children.length > 0) {
       let length = this.state.allItems.children.length;
       let currentBlot = this.state.allItems.children[length - 1];
-      currentBlot.scale(0.01  + this.expansionRate);
+      currentBlot.scale(0.01 + this.expansionRate);
       this.state.allItems.children[length - 1] = currentBlot;
+    }
+
+    if (this.state.allItems.children.length == 2) 
+    {
+      for (let i = 0; i < this.state.allItems.children[0].segments.length; i++)
+      {
+        this.state.allItems.children[0].segments[i].point += i / 20
+        //Toy example: checking to see whether anchor point can be altered
+      }
     }
   }
 
   render() //recalled every time setState is called elsewhere in the code
   {
-    return(
+    return (
       <div>
         <canvas id="canvas" width="1000" height="800"></canvas>
       </div>
