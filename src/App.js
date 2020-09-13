2f0734ae90746e6a7ce8002cf2db5b51a3aa36aa
samueljlopes <samueljlopes@outlook.com>
2020-09-13 14:17:15 +0100

    Added sidebar
    Had to override some properties inline, which is not recommended. Moved the canvas window into a seperate class.

--- src/App.js
+++ src/App.js
@@ -1,78 +1,45 @@
 import React, { Component } from 'react';
 import './App.css';
-import InkCanvas from './components/inkCanvas.js'
-import InkPhysics from './components/inkPhysics.js'
-import InkTineLines from './core/inkTineLines.js'
-import { Radio } from "antd";
+import InkWindow from './components/inkWindow.js'
 import 'antd/dist/antd.css';
-import paper from 'paper';
+import { Layout, Menu, Typography } from 'antd';
+import styled from 'styled-components';
+
+const { Content, Sider, } = Layout;
+const { Title } = Typography;
 
 class App extends Component {
   state = {
-    optionsDrawerValue: 0,
-    allItems: paper.Group
-  }
+    collapsed: false,
+  };
 
-  componentDidMount() {
-    paper.install(window);
-    this.setState({ allItems: new paper.Group() })
-  }
-
-  onChangeOptionsDrawerValue = (val) => {
-    this.setState({ optionsDrawerValue: val.target.value })
-  }
-
-  addItemToAllItems = (paperItem) => {
-    let currentAllItems = this.state.allItems;
-    currentAllItems.addChild(paperItem);
-    this.setState({ allItems: currentAllItems });
-  }
+  onCollapse = collapsed => {
+    console.log(collapsed);
+    this.setState({ collapsed });
+  };
 
   render() {
-    let currentCanvasTool;
-    switch(this.state.optionsDrawerValue) {
-      case 0:
-        currentCanvasTool = <InkPhysics expansionRate={1} 
-        allItems={this.state.allItems} addItemToAllItems={this.addItemToAllItems}></InkPhysics>
-        break;
-      case 1:
-        currentCanvasTool = <InkTineLines allItems={this.state.allItems} alpha={80} lambda={8}></InkTineLines>
-        break;
-      default:
-        currentCanvasTool = <InkPhysics expansionRate={1} 
-        allItems={this.state.allItems} addItemToAllItems={this.addItemToAllItems}></InkPhysics>
-    }
-
     return (
       <div>
-        <OptionsDrawer value={this.state.optionsDrawerValue} onChange={this.onChangeOptionsDrawerValue}></OptionsDrawer>
-        <div className="mainCanvas">
-          <InkCanvas></InkCanvas>
-          {currentCanvasTool}
-        </div>
+        <Layout style={{ minHeight: '100vh' }}>
+          <Sider /* collapsible collapsed={this.state.collapsed} onCollapse={this.onCollapse} */>
+            <Menu theme="dark" defaultSelectedKeys={['1']} mode="inline">
+              <Title style={{ padding: 8, color: "#FFFFFF"}}>marble.ink</Title>
+              <Menu.Item key="1">
+                Option 1
+            </Menu.Item>
+              <Menu.Item key="2">
+                Option 2
+            </Menu.Item>
+            </Menu>
+          </Sider>
+          <Layout className="site-layout">
+            <Content style={{ margin: '0px' }}>
+              <InkWindow></InkWindow>
+            </Content>
+          </Layout>
+        </Layout>
       </div>
-    );
-  }
-}
-
-class OptionsDrawer extends React.Component {
-  render() {
-    const { value, onChange } = this.props;
-    const radioStyle = {
-      display: "block",
-      height: "30px",
-      lineHeight: "30px"
-    };
-
-    return (
-      <Radio.Group onChange={onChange} value={value}>
-        <Radio.Button style={radioStyle} value={0}>
-          No Tine Lines
-        </Radio.Button>
-        <Radio.Button style={radioStyle} value={1}>
-          Tine Lines
-        </Radio.Button>
-      </Radio.Group>
     );
   }
 }
---
+++ src/components/inkWindow.js
@@ -0,0 +1,82 @@
+import React, { Component } from 'react';
+import './inkWindow.css';
+import InkCanvas from './inkCanvas.js'
+import InkPhysics from './inkPhysics.js'
+import InkTineLines from '../core/inkTineLines.js'
+import { Radio } from "antd";
+import 'antd/dist/antd.css';
+import paper from 'paper';
+
+class InkWindow extends Component {
+  state = {
+    optionsDrawerValue: 0,
+    allItems: paper.Group
+  }
+
+  componentDidMount() {
+    paper.install(window);
+    this.setState({ allItems: new paper.Group() })
+  }
+
+  onChangeOptionsDrawerValue = (val) => {
+    this.setState({ optionsDrawerValue: val.target.value })
+  }
+
+  addItemToAllItems = (paperItem) => {
+    let currentAllItems = this.state.allItems;
+    currentAllItems.addChild(paperItem);
+    this.setState({ allItems: currentAllItems });
+  }
+
+  render() {
+    let currentCanvasTool;
+    switch(this.state.optionsDrawerValue) {
+      case 0:
+        currentCanvasTool = <InkPhysics expansionRate={1} 
+        allItems={this.state.allItems} addItemToAllItems={this.addItemToAllItems}></InkPhysics>
+        break;
+      case 1:
+        currentCanvasTool = <InkTineLines allItems={this.state.allItems} alpha={80} lambda={8}></InkTineLines>
+        break;
+      default:
+        currentCanvasTool = <InkPhysics expansionRate={1} 
+        allItems={this.state.allItems} addItemToAllItems={this.addItemToAllItems}></InkPhysics>
+    }
+
+    return (
+      <div>
+        <div className="mainCanvas">
+          <InkCanvas></InkCanvas>
+          <div className="optionsDrawer">
+            <OptionsDrawer value={this.state.optionsDrawerValue} onChange={this.onChangeOptionsDrawerValue}></OptionsDrawer>
+          </div>
+          {currentCanvasTool}
+        </div>
+      </div>
+    );
+  }
+}
+
+class OptionsDrawer extends React.Component {
+  render() {
+    const { value, onChange } = this.props;
+    const radioStyle = {
+      display: "block",
+      height: "30px",
+      lineHeight: "30px"
+    };
+
+    return (
+      <Radio.Group onChange={onChange} value={value}>
+        <Radio.Button style={radioStyle} value={0}>
+          No Tine Lines
+        </Radio.Button>
+        <Radio.Button style={radioStyle} value={1}>
+          Tine Lines
+        </Radio.Button>
+      </Radio.Group>
+    );
+  }
+}
+
+export default InkWindow;
