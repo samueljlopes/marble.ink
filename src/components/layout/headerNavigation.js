b2ef8a394e3933c8efb4ea1026f590e39d78f679
samueljlopes <samueljlopes@outlook.com>
2021-08-21 00:01:10 +0100

    Added header menu
    Not going to bother attempting to directly modify the Ant Design theme so used a hacky workaround.

--- src/components/layout/headerNavigation.js
+++ src/components/layout/headerNavigation.js
@@ -2,21 +2,34 @@ import React, { Component } from 'react';
 
 import 'antd/dist/antd.css';
 import './styles/headerNavigation.css'
-import { Layout, Typography } from 'antd';
+import { Layout, Typography, Menu } from 'antd';
 
 const { Title } = Typography;
 const { Header } = Layout;
 
-class HeaderNavigation extends Component 
-{
-    render() {
-        return (
-          <div>
-              <Header>
-                <Title className="marbleTitle">marble.ink</Title>
-              </Header>
+class HeaderNavigation extends Component {
+  state = {
+    currentMenuItem: 0,
+  };
+
+  handleClick = e => {
+    this.setState({ currentmenuItem: e.key });
+  };
+
+  render() {
+    return (
+      <div>
+        <Header>
+          <div className="headerTitle">
+            <Title className="marbleFont">marble.ink</Title>
           </div>
-        );
-    }
+          <Menu theme="dark" className="headerMenu" mode="horizontal" defaultSelectedKeys={['0']}>
+              <Menu.Item key={0}>Canvas</Menu.Item>
+              <Menu.Item key={1}>About Me</Menu.Item>
+          </Menu>
+        </Header>
+      </div>
+    );
+  }
 }
 export default HeaderNavigation
