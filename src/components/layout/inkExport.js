30a3de7246c295027db374471f9dd195b6f2a08f
samueljlopes <samueljlopes@outlook.com>
2021-08-31 00:13:21 +0100

    Added some css to export as well as a bug fix

--- src/components/layout/inkExport.js
+++ src/components/layout/inkExport.js
@@ -2,9 +2,11 @@ import paper from 'paper'
 import React from 'react'
 
 import { Modal, Button } from 'antd';
+import './styles/inkExport.css'
 
 class InkExport extends React.Component {
     state = {
+        currentCanvasImage: paper.Raster,
         currentCanvasImageURL : ""
     }
 
@@ -17,6 +19,8 @@ class InkExport extends React.Component {
     {
         var tempImg = paper.project.activeLayer.rasterize();
         var dataString = tempImg.toDataURL();
+
+        this.setState({currentCanvasImage: tempImg})
         this.setState({currentCanvasImageURL: dataString})
     }
 
@@ -25,10 +29,16 @@ class InkExport extends React.Component {
             <div>
                 <Modal title="Export Your Artwork" visible={true} 
                 onOk={this.handleOk} onCancel={this.props.onCancel}>
-                <img src={this.state.currentCanvasImageURL} />
+                    <img src={this.state.currentCanvasImageURL} id='canvasImage'/>
+                    <p>You can share your creation on a number of social media platforms, including Instagram.</p>
                 </Modal>
             </div>
         );
+    }
+
+    componentWillUnmount() 
+    {
+        this.state.currentCanvasImage.remove();
     }
 }
 export default InkExport
